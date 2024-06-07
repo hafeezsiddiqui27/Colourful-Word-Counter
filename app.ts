e26145135c420e64a2a1855b23978317831ccb86
Hafeez Uddin Ahmed Siddiqui <hafeez27isbest@gmail.com>
2024-06-08 03:11:41 +0500

    Colourful word counter

---
+++ app.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+//importing inquirer and chalk packages
+import inquirer from "inquirer";
+import chalk from "chalk"
+
+//diplaying a colourful welcome messsage
+
+console.log(
+    chalk.bold.yellow(
+      `\n  \t\t <<<================================================>>>`
+    )
+  );
+  console.log(
+    chalk.bold.blue(
+      `<<<=========>>>  ${chalk.blueBright.bold(
+        "Welcome To 'Hafeez Siddiqui' - CLI TODO-LIST"
+      )}  <<<=========>>>`
+    )
+  );
+  console.log(
+    chalk.bold.yellow(
+      `\t\t <<<===============================================>>>\n`
+    )
+  );
+
+//prompt the user to enter
+let answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message:"Enter A Sentence:"
+    }
+])
+
+//trimming the answer and splitting the words based on spaces
+let words = answer.sentence.trim().split(" ")
+
+//Analysis of the input sentence and desplaying answer 
+console.log((chalk.yellowBright(`=`.repeat(70))))
+console.log(chalk.bold.greenBright("- sentence words:")) 
+console.log(words)
+console.log(chalk.bold.blueBright(`\n - Word Count: ${chalk.bold.redBright(words.length)}`))
+console.log((chalk.yellowBright(`=`.repeat(70))))
+
